cd23a88ea858edf80dc46fdca4f2538668be90c6
returntears <returntears@outlook.com>
2019-10-20 21:30:41 +0800

    consumer自测访问数据通过

---
+++ allst-microservice-consumer/src/main/java/com/yiya/allst/DeptConsumerApp.java
@@ -0,0 +1,17 @@
+package com.yiya.allst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class DeptConsumerApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(DeptConsumerApp.class);
+    }
+}
