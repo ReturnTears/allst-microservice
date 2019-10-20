1099d8f504a5b2162bf55ab098040329d690e867
returntears <returntears@outlook.com>
2019-10-20 21:30:28 +0800

    consumer自测访问数据通过

---
+++ allst-microservice-consumer/src/test/java/com/yiya/allst/AppTest.java
@@ -0,0 +1,20 @@
+package com.yiya.allst;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple DeptConsumerApp.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
